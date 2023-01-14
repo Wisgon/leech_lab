9411c684e25d12cdc22c39898f9bfc5f0f7bcf28
zhilong <wisgonchen@gmail.com>
2023-01-14 16:36:25 +0900

    theory markdown

--- brain/graph.go
+++ brain/graph.go
@@ -5,7 +5,7 @@ import gs "graph_robot/graph_structure"
 type Brain struct {
 	Eyes []gs.NetWork // 視覺神經的觸發神經元
 	// Ear  []gs.NetWork // 聽覺神經的觸發神經元
-	Mounth     []gs.NetWork // 閱讀文字的觸發神經元
+	Mouth      []gs.NetWork // word output network
 	ValueArea  []gs.NetWork // 價值判斷區，負責給與反饋，反饋大小取決於給與的電流強弱（分數的正負，負數越多越不好，到某種程度就是恐懼感,正數越大越好），是一組預先定義好的神經元,如果某個動作是正確的，則要把這個動作的神經元組連到這個區域中鞏固下來,以後想到這個動作就會得到這個value反饋
 	SenceArea  []gs.NetWork // 情景网络
 	MemoryArea []gs.NetWork // 记忆网络
